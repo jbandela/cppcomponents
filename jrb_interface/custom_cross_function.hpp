aad81ec7cf358f60e11f2d1374d4c8bc97342371
jbandela <jbandela@gmail.com>
2013-02-23 19:23:45 -0500

    Added ability for custom_function to use something besides std::function

--- jrb_interface/custom_cross_function.hpp
+++ jrb_interface/custom_cross_function.hpp
@@ -51,13 +51,13 @@ struct fn_ptr_helper<R(Parms...)>{
 
 
 
-template<class Iface, int Id,class F1, class F2,class Derived>
+template<class Iface, int Id,class F1, class F2,class Derived,class FuncType = std::function<F1>>
 struct custom_cross_function{};
 
 
 // For usage
-template<template<bool> class Iface, int Id,class F1, class F2,class Derived>
-struct custom_cross_function<Iface<false>,Id,F1,F2,Derived>{
+template<template<bool> class Iface, int Id,class F1, class F2,class Derived,class FuncType>
+struct custom_cross_function<Iface<false>,Id,F1,F2,Derived,FuncType>{
 private:
 	portable_base* pV_;
 public:
@@ -101,24 +101,16 @@ protected:
 
 
 // For implementation
-template<template<bool> class Iface, int Id,class F1, class F2,class Derived>
-struct custom_cross_function<Iface<true>,Id,F1,F2,Derived>{
+template<template<bool> class Iface, int Id,class F1, class F2,class Derived,class FuncType>
+struct custom_cross_function<Iface<true>,Id,F1,F2,Derived,FuncType>:public FuncType { // For empty base optimization in case FuncType is of 0 size
 private:
 		portable_base* pV_;
 public:
 
 		enum{N = Iface<true>::base_sz + Id};
-		std::function<F1> func_;
 	typedef typename std::function<F1>::result_type ret;
+
 	typedef typename fn_ptr_helper<F2>::fn_ptr_t vtable_fn_ptr_t;
-
-		template<class Func>
-		void set_function(Func f){
-			vtable_n_base* vn =(vtable_n_base*)pV_;
-
-			*( (std::function<F1>*)vn->pFunctions_[N]) = f;
-		}
-
 		template<class... Parms>
 		ret operator()(Parms... p){
 			return static_cast<Derived*>(this)->call_vtable_function(p...);
@@ -126,7 +118,7 @@ public:
 		template<int sz>
 		custom_cross_function(vtable_n<true,sz>* vn):pV_(vn->get_portable_base()){
 			static_assert(N<sz,"Interface::sz too small");
-			vn->template set_function<N>(&func_);
+			vn->template set_function<N>(static_cast<FuncType*>(this));
 			vn->template add<N>(Derived::vtable_func);
 		}
 
@@ -146,6 +138,13 @@ public:
 		}
 
 protected:
+
+	template<class Func>
+	void set_function(Func f){
+		vtable_n_base* vn =(vtable_n_base*)pV_;
+		*( (FuncType*)vn->pFunctions_[N]) = f;
+	}
+
 	vtable_fn_ptr_t get_vtable_fn(){
 		return (vtable_fn_ptr_t)pV_->vfptr[N];
 	}
@@ -158,10 +157,10 @@ protected:
 		portable_base* pV_;
 		helper(portable_base* p):pV_(p){}
 
-		std::function<F1>& get_function(){
+		FuncType& get_function(){
 			vtable_n_base* vn =(vtable_n_base*)pV_;
 
-			return *( (std::function<F1>*)vn->pFunctions_[N]);
+			return *( (FuncType*)vn->pFunctions_[N]);
 
 
 		}
