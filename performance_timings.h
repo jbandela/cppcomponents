d68718c7cea5087979168da61cf04f784fcae91f
jbandela <jbandela@gmail.com>
2013-03-06 17:00:27 -0500

    Entire solution now builds with MSVC

--- performance_timings.h
+++ performance_timings.h
@@ -4,8 +4,8 @@
 using namespace cross_compiler_interface;
 
 
-template<bool bImp>
-struct TestInterface1:public cross_compiler_interface::define_interface<bImp,5>{
+template<class bImp>
+struct TestInterface1:public cross_compiler_interface::define_interface<bImp>{
 	cross_function<TestInterface1,0,void()>f0;
 	cross_function<TestInterface1,1,int()> f1;
 	cross_function<TestInterface1,2,int(int)> f2;
@@ -13,8 +13,7 @@ struct TestInterface1:public cross_compiler_interface::define_interface<bImp,5>{
 	cross_function<TestInterface1,4,void(std::string)>f4;
 
 
-	template<class T>
-	TestInterface1(T t):TestInterface1::base_t(t),f0(this),f1(this),f2(this),f3(this),f4(this){
+	TestInterface1():f0(this),f1(this),f2(this),f3(this),f4(this){
 	}
 
 
