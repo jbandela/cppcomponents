50183d57cfc2bfe36189d53d0f18522d59fcca8e
John Bandela <jbandela@gmail.com>
2013-04-06 12:41:38 -0400

    changes to simple demo

--- simple_demo.h
+++ simple_demo.h
@@ -4,11 +4,11 @@
 
 
 struct KVStore{
-	KVStore();
-	~KVStore();
-	void Put(const std::string& key, const std::string& value);
-	bool Get(const std::string& key,std::string* value);
-	bool Delete(const std::string& key);
+    KVStore();
+    ~KVStore();
+    void Put(const std::string& key, const std::string& value);
+    bool Get(const std::string& key,std::string* value);
+    bool Delete(const std::string& key);
 };
 
 #endif
@@ -21,33 +21,31 @@ typedef std::int32_t error_code;
 #define CALLING_CONVENTION __stdcall
 
 extern "C"{
-	HKVStore CALLING_CONVENTION Create_KVStore();
-	void CALLING_CONVENTION Destroy_KVStore(HKVStore h);
-
-	error_code CALLING_CONVENTION Put (HKVStore h,const char* key, int32_t key_count,const char* value, int32_t value_count);
-	error_code CALLING_CONVENTION Get(HKVStore h, const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn);
-	error_code CALLING_CONVENTION Delete(HKVStore h, const char* key, int32_t key_count,char* breturn);
-
-};
+    HKVStore CALLING_CONVENTION Create_KVStore();
+    void CALLING_CONVENTION Destroy_KVStore(HKVStore h);
+    error_code CALLING_CONVENTION Put (HKVStore h,const char* key, int32_t key_count,const char* value, int32_t value_count);
+    error_code CALLING_CONVENTION Get(HKVStore h, const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn);
+    error_code CALLING_CONVENTION Delete(HKVStore h, const char* key, int32_t key_count,char* breturn);
+}
 
 struct IKVStore{
-	virtual error_code CALLING_CONVENTION Put (const char* key, int32_t key_count,const char* value, int32_t value_count) = 0;
-	virtual error_code CALLING_CONVENTION Get(const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn) = 0;
-	virtual error_code CALLING_CONVENTION Delete(const char* key, int32_t key_count,char* breturn)=0;
-	virtual void CALLING_CONVENTION Destroy() = 0;
+    virtual error_code CALLING_CONVENTION Put (const char* key, int32_t key_count,const char* value, int32_t value_count) = 0;
+    virtual error_code CALLING_CONVENTION Get(const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn) = 0;
+    virtual error_code CALLING_CONVENTION Delete(const char* key, int32_t key_count,char* breturn)=0;
+    virtual void CALLING_CONVENTION Destroy() = 0;
 };
 
 struct IKVStore2;
 
 struct IKVStoreVtable{
-	error_code (CALLING_CONVENTION * Put) (IKVStore2* ikv, const char* key, int32_t key_count,const char* value, int32_t value_count);
-	error_code (CALLING_CONVENTION *Get)(IKVStore2* ikv, const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn);
-	error_code (CALLING_CONVENTION *Delete)(IKVStore2* ikv, const char* key, int32_t key_count,char* breturn);
-	void (CALLING_CONVENTION *Destroy)(IKVStore2* ikv);
+    error_code (CALLING_CONVENTION * Put) (IKVStore2* ikv, const char* key, int32_t key_count,const char* value, int32_t value_count);
+    error_code (CALLING_CONVENTION *Get)(IKVStore2* ikv, const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn);
+    error_code (CALLING_CONVENTION *Delete)(IKVStore2* ikv, const char* key, int32_t key_count,char* breturn);
+    void (CALLING_CONVENTION *Destroy)(IKVStore2* ikv);
 };
 
 struct IKVStore2{
-	IKVStoreVtable* vtable;
+    IKVStoreVtable* vtable;
 };
 
 
@@ -55,34 +53,34 @@ using cross_compiler_interface::cross_function;
 
 template<class T>
 struct InterfaceKVStore:public cross_compiler_interface::define_interface<T>{
-	cross_function<InterfaceKVStore,0,void(std::string,std::string)> Put;
-	cross_function<InterfaceKVStore,1,bool(std::string,cross_compiler_interface::out<std::string>)> Get;
-	cross_function<InterfaceKVStore,2,bool(std::string)> Delete;
-	cross_function<InterfaceKVStore,3,void()> Destroy;
-	
-	InterfaceKVStore()
-		:Put(this),Get(this),Delete(this),Destroy(this)
-	{}
+    cross_function<InterfaceKVStore,0,void(std::string,std::string)> Put;
+    cross_function<InterfaceKVStore,1,bool(std::string,cross_compiler_interface::out<std::string>)> Get;
+    cross_function<InterfaceKVStore,2,bool(std::string)> Delete;
+    cross_function<InterfaceKVStore,3,void()> Destroy;
+    
+    InterfaceKVStore()
+        :Put(this),Get(this),Delete(this),Destroy(this)
+    {}
 };
 
 template<class T>
 struct InterfaceKVStore2
-	:public cross_compiler_interface::define_interface_unknown<T,
-	// {B781B4FF-995D-4122-842C-E14A4C0348CC}
-	cross_compiler_interface::uuid<
-	0xB781B4FF,0x995D,0x4122,0x84,0x2C,0xE1,0x4A,0x4C,0x03,0x48,0xCC
-	>
-	>
+    :public cross_compiler_interface::define_interface_unknown<T,
+    // {B781B4FF-995D-4122-842C-E14A4C0348CC}
+    cross_compiler_interface::uuid<
+    0xB781B4FF,0x995D,0x4122,0x84,0x2C,0xE1,0x4A,0x4C,0x03,0x48,0xCC
+    >
+    >
 {
 
-	typedef cross_compiler_interface::cr_string cr_string;
-	cross_function<InterfaceKVStore2,0,void(cr_string,cr_string)> Put;
-	cross_function<InterfaceKVStore2,1,bool(cr_string,cross_compiler_interface::out<std::string>)> Get;
-	cross_function<InterfaceKVStore2,2,bool(cr_string)> Delete;
-	
-	InterfaceKVStore2()
-		:Put(this),Get(this),Delete(this)
-	{}
+    typedef cross_compiler_interface::cr_string cr_string;
+    cross_function<InterfaceKVStore2,0,void(cr_string,cr_string)> Put;
+    cross_function<InterfaceKVStore2,1,bool(cr_string,cross_compiler_interface::out<std::string>)> Get;
+    cross_function<InterfaceKVStore2,2,bool(cr_string)> Delete;
+    
+    InterfaceKVStore2()
+        :Put(this),Get(this),Delete(this)
+    {}
 
 
 };
--- simple_demo_dll.cpp
+++ simple_demo_dll.cpp
@@ -5,348 +5,332 @@
 
 // Extern C
 struct KVStore{
-	std::map<std::string,std::string> m_;
+    std::map<std::string,std::string> m_;
 };
 
 
 extern "C"{
-	HKVStore CALLING_CONVENTION Create_KVStore(){
-		try{
-			return new KVStore;
-		}
-		catch(std::exception&){
-			return nullptr;
-		}
+    HKVStore CALLING_CONVENTION Create_KVStore(){
+        try{
+            return new KVStore;
+        }
+        catch(std::exception&){
+            return nullptr;
+        }
+    }
 
-	}
-	void CALLING_CONVENTION Destroy_KVStore(HKVStore h){
-		delete h;
-	}
+    void CALLING_CONVENTION Destroy_KVStore(HKVStore h){
+        delete h;
+    }
 
-	error_code CALLING_CONVENTION Put (HKVStore h,const char* key, int32_t key_count,const char* value, int32_t value_count){
-		try{
-			std::string key(key,key_count);
-			std::string value(value,value_count);
-			h->m_[key] = value;
-			return 0;
-
-		}
-		catch(std::exception&){
-			return -1;
-		}
-	}
-	error_code CALLING_CONVENTION Get(HKVStore h, const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn){
-		try{
-			std::string key(key,key_count);
-			auto iter = h->m_.find(key);
-			if(iter == h->m_.end()){
-				*breturn = 0;
-				return 0;
-			}
-			else{
-				std::string value = iter->second;
-				auto pc = new char[value.size()];
-				std::copy(value.begin(),value.end(),pc);
-				*pvalue_count = value.size();
-				*pvalue = pc;
-
-				return 0;
-			}
-
-		}
-		catch(std::exception&){
-			return -1;
-		}
-
-	}
-	error_code CALLING_CONVENTION Delete(HKVStore h, const char* key, int32_t key_count,char* breturn){
-		try{
-			std::string key(key,key_count);
-			auto iter = h->m_.find(key);
-			if(iter == h->m_.end()){
-				*breturn = 0;
-				return 0;
-			}
-			else{
-				h->m_.erase(iter);
-
-				return 0;
-			}
-
-		}
-		catch(std::exception&){
-			return -1;
-		}
-	}
-
+    error_code CALLING_CONVENTION Put (HKVStore h,const char* key, int32_t key_count,const char* value, int32_t value_count){
+        try{
+            std::string key(key,key_count);
+            std::string value(value,value_count);
+            h->m_[key] = value;
+            return 0;
+        }
+        catch(std::exception&){
+            return -1;
+        }
+    }
+    error_code CALLING_CONVENTION Get(HKVStore h, const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn){
+        try{
+            std::string key(key,key_count);
+            auto iter = h->m_.find(key);
+            if(iter == h->m_.end()){
+                *breturn = 0;
+                return 0;
+            }
+            else{
+                std::string value = iter->second;
+                auto pc = new char[value.size()];
+                std::copy(value.begin(),value.end(),pc);
+                *pvalue_count = value.size();
+                *pvalue = pc;
+                return 0;
+            }
+        }
+        catch(std::exception&){
+            return -1;
+        }
+    }
+    error_code CALLING_CONVENTION Delete(HKVStore h, const char* key, int32_t key_count,char* breturn){
+        try{
+            std::string key(key,key_count);
+            auto iter = h->m_.find(key);
+            if(iter == h->m_.end()){
+                *breturn = 0;
+                return 0;
+            }
+            else{
+                h->m_.erase(iter);
+                return 0;
+            }
+        }
+        catch(std::exception&){
+            return -1;
+        }
+    }
 }
 
 // Compiler Vtable
 
 struct KVStoreImplementation:public IKVStore{
-	std::map<std::string,std::string> m_;
-	virtual error_code CALLING_CONVENTION Put (const char* key, int32_t key_count,const char* value, int32_t value_count) override{
-		try{
-			std::string key(key,key_count);
-			std::string value(value,value_count);
-			m_[key] = value;
-			return 0;
-		}
-		catch(std::exception&){
-			return -1;
-		}
-	}
-	virtual error_code CALLING_CONVENTION Get(const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn) override{
-		try{
-			std::string key(key,key_count);
-			auto iter = m_.find(key);
-			if(iter == m_.end()){
-				*breturn = 0;
-				return 0;
-			}
-			else{
-				std::string value = iter->second;
-				auto pc = new char[value.size()];
-				std::copy(value.begin(),value.end(),pc);
-				*pvalue_count = value.size();
-				*pvalue = pc;
+    std::map<std::string,std::string> m_;
 
-				return 0;
-			}
+    virtual error_code CALLING_CONVENTION Put (const char* key, int32_t key_count,const char* value, int32_t value_count) override{
+        try{
+            std::string key(key,key_count);
+            std::string value(value,value_count);
+            m_[key] = value;
+            return 0;
+        }
+        catch(std::exception&){
+            return -1;
+        }
+    }
+    virtual error_code CALLING_CONVENTION Get(const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn) override{
+        try{
+            std::string key(key,key_count);
+            auto iter = m_.find(key);
+            if(iter == m_.end()){
+                *breturn = 0;
+                return 0;
+            }
+            else{
+                std::string value = iter->second;
+                auto pc = new char[value.size()];
+                std::copy(value.begin(),value.end(),pc);
+                *pvalue_count = value.size();
+                *pvalue = pc;
+                return 0;
+            }
+        }
+        catch(std::exception&){
+            return -1;
+        }
+    }
 
-		}
-		catch(std::exception&){
-			return -1;
-		}
+    virtual error_code CALLING_CONVENTION Delete(const char* key, int32_t key_count,char* breturn)override{
+        try{
+            std::string key(key,key_count);
+            auto iter = m_.find(key);
+            if(iter == m_.end()){
+                *breturn = 0;
+                return 0;
+            }
+            else{
+                m_.erase(iter);
+                return 0;
+            }
+        }
+        catch(std::exception&){
+            return -1;
+        }
+    }
 
-	}
-
-
-	
-	virtual error_code CALLING_CONVENTION Delete(const char* key, int32_t key_count,char* breturn)override{
-		try{
-			std::string key(key,key_count);
-			auto iter = m_.find(key);
-			if(iter == m_.end()){
-				*breturn = 0;
-				return 0;
-			}
-			else{
-				m_.erase(iter);
-
-				return 0;
-			}
-
-		}
-		catch(std::exception&){
-			return -1;
-		}
-	}
-
-	virtual void CALLING_CONVENTION Destroy() override {
-		delete this;
-	}
-	
-
-
+    virtual void CALLING_CONVENTION Destroy() override {
+        delete this;
+    }
 };
 
 extern "C"{
-	IKVStore* CALLING_CONVENTION Create_KVStoreImplementation(){
-		try{
-			return new KVStoreImplementation;
-		}
-		catch(std::exception&){
-			return nullptr;
-		}
-
-	}
-
+    IKVStore* CALLING_CONVENTION Create_KVStoreImplementation(){
+        try{
+            return new KVStoreImplementation;
+        }
+        catch(std::exception&){
+            return nullptr;
+        }
+    }
 }
 
 // Programmer generated vtable
 
 struct KVStore2Implementation:public IKVStore2{
-	std::map<std::string,std::string> m_;
-	IKVStoreVtable vt;
+    std::map<std::string,std::string> m_;
+    IKVStoreVtable vt;
+
+    KVStore2Implementation(){
+        vtable = &vt;
+        vtable->Put = &Put_;
+        vtable->Get = &Get_;
+        vtable->Delete = &Delete_;
+        vtable->Destroy = &Destroy_;
+    }
 
 
-	static void CALLING_CONVENTION Destroy_(IKVStore2* ikv ){
-		delete static_cast<KVStore2Implementation*>(ikv);
-	}
+    static void CALLING_CONVENTION Destroy_(IKVStore2* ikv ){
+        delete static_cast<KVStore2Implementation*>(ikv);
+    }
 
-	static error_code CALLING_CONVENTION Put_ (IKVStore2* ikv, const char* key, int32_t key_count,const char* value, int32_t value_count){
-		try{
-			std::string key(key,key_count);
-			std::string value(value,value_count);
-			static_cast<KVStore2Implementation*>(ikv)->m_[key] = value;
-			return 0;
+    static error_code CALLING_CONVENTION Put_ (IKVStore2* ikv, const char* key, int32_t key_count,const char* value, int32_t value_count){
+        try{
+            std::string key(key,key_count);
+            std::string value(value,value_count);
+            static_cast<KVStore2Implementation*>(ikv)->m_[key] = value;
+            return 0;
 
-		}
-		catch(std::exception&){
-			return -1;
-		}
-	}
-	static error_code CALLING_CONVENTION Get_(IKVStore2* ikv,  const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn){
-		try{
-			std::string key(key,key_count);
-			auto iter = static_cast<KVStore2Implementation*>(ikv)->m_.find(key);
-			if(iter == static_cast<KVStore2Implementation*>(ikv)->m_.end()){
-				*breturn = 0;
-				return 0;
-			}
-			else{
-				std::string value = iter->second;
-				auto pc = new char[value.size()];
-				std::copy(value.begin(),value.end(),pc);
-				*pvalue_count = value.size();
-				*pvalue = pc;
+        }
+        catch(std::exception&){
+            return -1;
+        }
+    }
+    static error_code CALLING_CONVENTION Get_(IKVStore2* ikv,  const char* key, int32_t key_count,const char** pvalue,int32_t* pvalue_count,char* breturn){
+        try{
+            std::string key(key,key_count);
+            auto iter = static_cast<KVStore2Implementation*>(ikv)->m_.find(key);
+            if(iter == static_cast<KVStore2Implementation*>(ikv)->m_.end()){
+                *breturn = 0;
+                return 0;
+            }
+            else{
+                std::string value = iter->second;
+                auto pc = new char[value.size()];
+                std::copy(value.begin(),value.end(),pc);
+                *pvalue_count = value.size();
+                *pvalue = pc;
 
-				return 0;
-			}
+                return 0;
+            }
 
-		}
-		catch(std::exception&){
-			return -1;
-		}
+        }
+        catch(std::exception&){
+            return -1;
+        }
 
-	}
-	static error_code CALLING_CONVENTION Delete_(IKVStore2* ikv,  const char* key, int32_t key_count,char* breturn){
-		try{
-			std::string key(key,key_count);
-			auto iter = static_cast<KVStore2Implementation*>(ikv)->m_.find(key);
-			if(iter == static_cast<KVStore2Implementation*>(ikv)->m_.end()){
-				*breturn = 0;
-				return 0;
-			}
-			else{
-				static_cast<KVStore2Implementation*>(ikv)->m_.erase(iter);
+    }
+    static error_code CALLING_CONVENTION Delete_(IKVStore2* ikv,  const char* key, int32_t key_count,char* breturn){
+        try{
+            std::string key(key,key_count);
+            auto iter = static_cast<KVStore2Implementation*>(ikv)->m_.find(key);
+            if(iter == static_cast<KVStore2Implementation*>(ikv)->m_.end()){
+                *breturn = 0;
+                return 0;
+            }
+            else{
+                static_cast<KVStore2Implementation*>(ikv)->m_.erase(iter);
 
-				return 0;
-			}
+                return 0;
+            }
 
-		}
-		catch(std::exception&){
-			return -1;
-		}
-	}
+        }
+        catch(std::exception&){
+            return -1;
+        }
+    }
 
-	KVStore2Implementation(){
-		vtable = &vt;
-		vtable->Put = &Put_;
-		vtable->Get = &Get_;
-		vtable->Delete = &Delete_;
-		vtable->Destroy = &Destroy_;
-	}
+
 
 };
 
 extern "C"{
-	IKVStore2* CALLING_CONVENTION Create_KVStore2Implementation(){
-		try{
-			return new KVStore2Implementation;
-		}
-		catch(std::exception&){
-			return nullptr;
-		}
+    IKVStore2* CALLING_CONVENTION Create_KVStore2Implementation(){
+        try{
+            return new KVStore2Implementation;
+        }
+        catch(std::exception&){
+            return nullptr;
+        }
 
-	}
+    }
 
 }
 
 
 struct ImplementKVStore{
-	cross_compiler_interface::implement_interface<InterfaceKVStore> imp_;
+    cross_compiler_interface::implement_interface<InterfaceKVStore> imp_;
 
-	std::map<std::string,std::string> m_;
+    std::map<std::string,std::string> m_;
 
-	ImplementKVStore(){
+    ImplementKVStore(){
 
-		imp_.Put = [this](std::string key, std::string value){
-			m_[key] = value;
-		};
+        imp_.Put = [this](std::string key, std::string value){
+            m_[key] = value;
+        };
 
-		imp_.Get = [this](std::string key, cross_compiler_interface::out<std::string> value){
-			auto iter = m_.find(key);
-			if(iter==m_.end()) return false;
-			value.set(iter->second);
-			return true;
-		};
+        imp_.Get = [this](std::string key, cross_compiler_interface::out<std::string> value){
+            auto iter = m_.find(key);
+            if(iter==m_.end()) return false;
+            value.set(iter->second);
+            return true;
+        };
 
-		imp_.Delete = [this](std::string key){
-			auto iter = m_.find(key);
-			if(iter==m_.end())return false;
-			m_.erase(iter);
-			return true;
-		};
-		
-		imp_.Destroy = [this](){
-			delete this;
-		};
+        imp_.Delete = [this](std::string key){
+            auto iter = m_.find(key);
+            if(iter==m_.end())return false;
+            m_.erase(iter);
+            return true;
+        };
+        
+        imp_.Destroy = [this](){
+            delete this;
+        };
 
 
-	}
+    }
 
 
 };
 extern "C"{
-	cross_compiler_interface::portable_base* CALLING_CONVENTION Create_ImplementKVStore(){
-		try{
-			auto p = new ImplementKVStore;
-			return p->imp_.get_portable_base();
-		}
-		catch(std::exception&){
-			return nullptr;
-		}
+    cross_compiler_interface::portable_base* CALLING_CONVENTION Create_ImplementKVStore(){
+        try{
+            auto p = new ImplementKVStore;
+            return p->imp_.get_portable_base();
+        }
+        catch(std::exception&){
+            return nullptr;
+        }
 
-	}
+    }
 
 }
 
 
 struct ImplementKVStore2
-	:public cross_compiler_interface::implement_unknown_interfaces<ImplementKVStore2,InterfaceKVStore2>
+    :public cross_compiler_interface::implement_unknown_interfaces<ImplementKVStore2,InterfaceKVStore2>
 
 {
 
-	std::map<std::string,std::string> m_;
+    std::map<std::string,std::string> m_;
 
-	ImplementKVStore2(){
-		using cross_compiler_interface::cr_string;
-		auto imp = get_implementation<InterfaceKVStore2>();
+    ImplementKVStore2(){
+        using cross_compiler_interface::cr_string;
+        auto imp = get_implementation<InterfaceKVStore2>();
 
-		imp->Put = [this](cr_string key, cr_string value){
-			m_[key.to_string()] = value.to_string();
-		};
+        imp->Put = [this](cr_string key, cr_string value){
+            m_[key.to_string()] = value.to_string();
+        };
 
-		imp->Get = [this](cr_string key, cross_compiler_interface::out<std::string> value){
-			auto iter = m_.find(key.to_string());
-			if(iter==m_.end()) return false;
-			value.set(iter->second);
-			return true;
-		};
+        imp->Get = [this](cr_string key, cross_compiler_interface::out<std::string> value){
+            auto iter = m_.find(key.to_string());
+            if(iter==m_.end()) return false;
+            value.set(iter->second);
+            return true;
+        };
 
-		imp->Delete = [this](cr_string key){
-			auto iter = m_.find(key.to_string());
-			if(iter==m_.end())return false;
-			m_.erase(iter);
-			return true;
-		};
+        imp->Delete = [this](cr_string key){
+            auto iter = m_.find(key.to_string());
+            if(iter==m_.end())return false;
+            m_.erase(iter);
+            return true;
+        };
 
-	}
+    }
 
 
 };
 extern "C"{
-	cross_compiler_interface::portable_base* CALLING_CONVENTION Create_ImplementKVStore2(){
-		try{
-			auto p = ImplementKVStore2::create();
-			return p.get_portable_base_addref();
-		}
-		catch(std::exception&){
-			return nullptr;
-		}
+    cross_compiler_interface::portable_base* CALLING_CONVENTION Create_ImplementKVStore2(){
+        try{
+            auto p = ImplementKVStore2::create();
+            return p.get_portable_base_addref();
+        }
+        catch(std::exception&){
+            return nullptr;
+        }
 
-	}
+    }
 
 }
