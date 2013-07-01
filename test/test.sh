5e16c86d88449cd0630cfc0bad59d43a638c5328
John Bandela <jrb@vps.server.com>
2013-07-01 12:36:49 -0400

    Added automated test for linux

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 echo "Building clang .so"
-clang++ -std=c++11 -stdlib=libc++ ../unit_test_dll.cpp -shared -o unit_test_dll.so -fvisibility=hidden
+clang++ -std=c++11 -stdlib=libc++ ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC
 
 echo "Building g++ executable"
-g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external\googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe
+g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread
 
 
 echo "Running g++(exe) with clang++(so)"
 
-./unit_test_exe.exe --gtest_print_time=0
+./unit_test_exe --gtest_print_time=0
 
-rm *.o
 rm *.so
 rm unit_test_exe
+
+
+echo "Building g++ .so"
+
+g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC
+
+echo "Building clang executable"
+
+clang++ -std=c++11 -stdlib=libc++ -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread -lsupc++
+echo "Running clang++(exe) with g++(so)"
+
+./unit_test_exe --gtest_print_time=0
+
+rm *.so
+rm unit_test_exe
+
 
