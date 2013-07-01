5b7663dd29df0a7161386e208276cf6fe1d4701d
John Bandela <jrb@vps.server.com>
2013-07-01 14:48:53 -0400

    Allowed -fvisibility=hidden to be used

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Building clang .so"
-clang++ -std=c++11 -stdlib=libc++ ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC
+clang++ -std=c++11 -stdlib=libc++ ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden
 
 echo "Building g++ executable"
 g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread
@@ -17,7 +17,7 @@ rm unit_test_exe
 
 echo "Building g++ .so"
 
-g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC
+g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden
 
 echo "Building clang executable"
 
