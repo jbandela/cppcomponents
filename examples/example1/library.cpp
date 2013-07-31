ee5e4d00f40bf55419257bde9276812adac53088
John Bandela <jbandela@gmail.com>
2013-07-31 18:08:27 -0400

    Updated example to lastest cppcomponent changes

--- examples/example1/library.cpp
+++ examples/example1/library.cpp
@@ -5,6 +5,12 @@ struct PersonImplementation:cppcomponents::implement_runtime_class<PersonImpleme
 
     std::string SayHello(){return "Hello World\n";}
 
+    // Make sure you have at least 1 constructor
+    // If not, the class will not be automatically registered
+    // With the class factory
+
+    PersonImplementation(){}
+
 };
 
-CPPCOMPONENTS_DEFINE_FACTORY(PersonImplementation);
+CPPCOMPONENTS_DEFINE_FACTORY();
