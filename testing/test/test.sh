d4725c4444dc59426069474e350a7fc4f775696c
John Bandela <jrb@vps.server.com>
2013-08-02 08:22:40 -0400

    Updated test.sh to run g++(exe) with g+=(so) to test out futures

--- testing/test/test.sh
+++ testing/test/test.sh
@@ -34,6 +34,11 @@ g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibili
 
 echo "Building g++ executable"
 g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread
+echo "Running g++(exe) with g++(so)"
+
+./unit_test_exe --gtest_print_time=0
+
+
 rm *.so
 rm unit_test_exe
 
