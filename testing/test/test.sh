1b031951996b3bc2a84281385e5a1aa89fcc7ff1
John Bandela <jbandela@gmail.com>
2013-08-02 12:33:30 -0400

    Support for using boost futures

--- testing/test/test.sh
+++ testing/test/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Building clang .so"
-clang++ -std=c++11 -stdlib=libc++ ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden
+clang++ -std=c++11 -stdlib=libc++ ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden -DCPPCOMPONENTS_USE_BOOST_FUTURE -lboost_thread -lboost_system -pthread
 
 echo "Building g++ executable"
-g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread -DCPPCOMPONENTS_TEST_NO_FUTURES
+g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread 
 
 
 echo "Running g++(exe) with clang++(so)"
@@ -17,12 +17,11 @@ rm unit_test_exe
 
 echo "Building g++ .so"
 
-g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden
+g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden -pthread
 
 echo "Building clang executable"
 
-clang++ -std=c++11 -stdlib=libc++ -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread -lsupc++ -DCPPCOMPONENTS_TEST_NO_FUTURES
-echo "Running clang++(exe) with g++(so)"
+clang++ -std=c++11 -stdlib=libc++ -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread -lsupc++ -DCPPCOMPONENTS_USE_BOOST_FUTURE -lboost_thread -lboost_system
 
 ./unit_test_exe --gtest_print_time=0
 
