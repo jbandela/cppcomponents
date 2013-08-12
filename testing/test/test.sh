f74b05487ffd68f393f846a6b017b5848444f4e1
John Bandela <jrb@vps.server.com>
2013-08-12 16:01:53 -0400

    Simplified test.sh

--- testing/test/test.sh
+++ testing/test/test.sh
@@ -4,7 +4,7 @@ echo "Building clang .so"
 clang++ -std=c++11 -stdlib=libc++ ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden
 
 echo "Building g++ executable"
-g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread -DCPPCOMPONENTS_TEST_NO_FUTURES
+g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread 
 
 
 echo "Running g++(exe) with clang++(so)"
@@ -21,23 +21,10 @@ g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibili
 
 echo "Building clang executable"
 
-clang++ -std=c++11 -stdlib=libc++ -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread -lsupc++ -DCPPCOMPONENTS_TEST_NO_FUTURES
+clang++ -std=c++11 -stdlib=libc++ -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread -lsupc++ 
 echo "Running clang++(exe) with g++(so)"
 
 ./unit_test_exe --gtest_print_time=0
-
-rm *.so
-rm unit_test_exe
-echo "Building g++ .so"
-
-g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden
-
-echo "Building g++ executable"
-g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread
-echo "Running g++(exe) with g++(so)"
-
-./unit_test_exe --gtest_print_time=0
-
 
 rm *.so
 rm unit_test_exe
