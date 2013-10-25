8eed546b7137a5db6f94ef93e7795a5c25b02b5b
John Bandela <jrb@vps.server.com>
2013-10-25 18:18:48 -0400

    Fixed some warning compilations problems on linux

--- testing/test/test.sh
+++ testing/test/test.sh
@@ -6,7 +6,7 @@ echo "Building clang .so"
 clang++ -std=c++11  -stdlib=$clang_lib ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_1 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_2 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8  -Werror -Wpedantic -pedantic-errors
 
 echo "Building g++ executable"
-g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ../unit_test_exe_when_all.cpp ../unit_test_exe_when_any.cpp ../unit_test_exe_when_any_tuple.cpp ../unit_test_exe_channel.cpp  ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread  -Werror -Wpedantic -pedantic-errors
+g++ -std=c++11 -U__STRICT_ANSI__ ../unit_test_exe.cpp ../unit_test_exe_when_all.cpp ../unit_test_exe_when_any.cpp ../unit_test_exe_when_any_tuple.cpp ../unit_test_exe_channel.cpp  ./external/googletest-read-only/src/gtest_main.cc ./external/googletest-read-only/src/gtest-all.cc -I ./external/googletest-read-only -I ./external/googletest-read-only/include -o unit_test_exe -ldl -pthread  
 
 
 echo "Running g++(exe) with clang++(so)"
@@ -19,7 +19,7 @@ rm unit_test_exe
 
 echo "Building g++ .so"
 
-g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden  -Werror -Wpedantic -pedantic-errors
+g++ -std=c++11 ../unit_test_dll.cpp -shared -o unit_test_dll.so -fPIC -fvisibility=hidden  
 
 echo "Building clang executable"
 
