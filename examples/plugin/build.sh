1207cb633a0ebea25a94b8f74944737614274657
jbandela <jbandela@gmail.com>
2014-01-04 22:17:06 -0500

    updated linux build script for plugin example

--- examples/plugin/build.sh
+++ examples/plugin/build.sh
@@ -1,4 +1,4 @@
-g++ -std=c++11 -I ../../ main.cpp -o main.exe -ldl -pthread
+g++ -std=c++11 -I ../../ main.cpp -o main -ldl -pthread
 
 clang++ -std=c++11 -I ../../ plugin1.cpp -shared -fPIC -o plugin1.so
 clang++ -std=c++11 -I ../../ plugin2.cpp -shared -fPIC -o plugin2.so
